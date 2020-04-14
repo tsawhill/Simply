a649c355ff4d652ddd1182ca251510e3ca72bd5b
Trevor Sawhill <trevor.s.sawhill@gmail.com>
2020-04-14 16:17:05 -0700

    Add now playing page

--- lib/nowplaying.dart
+++ lib/nowplaying.dart
@@ -1,6 +1,9 @@
 import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:simply_music/mainscreen.dart';
 
 class NowPlaying extends StatefulWidget {
   @override
@@ -14,6 +17,20 @@ class NowPlayingState extends State<NowPlaying> {
   Widget build(BuildContext context){
     return Column(
       children: <Widget>[
+        StreamBuilder<MediaItem>(
+          stream: AudioService.currentMediaItemStream,
+          builder: (context, snapshot) {
+            final screenState = snapshot.data;
+            if(!snapshot.hasData){
+              return Container(child: Text('No song playing'));
+            }
+            else{
+              return Container(
+                child: Text('$screenState.title'),
+              );
+            }
+          },
+        )
         ]
     );
   }
