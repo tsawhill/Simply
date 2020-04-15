1998f184a005956df1e863bf2cdf952085818556
Trevor Sawhill <trevor.s.sawhill@gmail.com>
2020-04-15 12:56:04 -0700

    Add list of albums grabbed by audio query, eventually handle all at startup

--- lib/album.dart
+++ lib/album.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
 
-class AlbumPage extends StatelessWidget {
+class AlbumPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return Container(
-      padding: EdgeInsets.all(25),
-      child: Text('Albums'),
+  State<StatefulWidget> createState() {
+    return AlbumPageState();
+  }
+}
+class AlbumPageState extends State<AlbumPage> {
+  final audioQuery = FlutterAudioQuery();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getAlbums(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: Text('No songs found!'));
+          }
+          else
+            return Container(
+                child: ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return FlatButton(
+                        child: Text('${snapshot.data[index].title}'),
+                        onPressed: () {
+
+                        },
+
+
+                      );
+                    }
+                )
+            );
+        }
     );
+  }
+
+  Future<List> getAlbums() async {
+    return audioQuery.getAlbums();
   }
 }
