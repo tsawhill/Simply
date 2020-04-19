7f5e0bc43a005e60a4cae870ff1aee08860995e8
Trevor Sawhill <trevor.s.sawhill@gmail.com>
2020-04-18 17:02:38 -0700

    Replace flutter_audio_service with flute_music_player

---
+++ lib/audioplayer.dart
@@ -0,0 +1,2 @@
+import 'package:flute_music_player/flute_music_player.dart';
+MusicFinder audioPlayer;
--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,10 @@
-import 'dart:async';
 import 'mainscreen.dart';
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:audio_service/audio_service.dart';
 
-void musicEntry() async{
-  AudioServiceBackground.run(() => AudioPlayerTask());
-}
-
-class AudioPlayerTask extends BackgroundAudioTask {
-  final audioPlayer = AudioPlayer();
-  final completer = Completer();
-
-  @override
-  Future<void> onStart() async {
-    await completer.future;
-  }
-
-  @override
-  void onStop() {
-    audioPlayer.stop();
-    completer.complete();
-  }
-}
 
 
 void main() async {
-  AudioService.start(backgroundTaskEntrypoint: musicEntry,
-    androidNotificationChannelName: 'Simply Music',
-    notificationColor: 0xFF2196f3,
-    enableQueue: false,);
   runApp(MyApp());
-
 }
 
 class MyApp extends StatefulWidget {
@@ -45,9 +18,9 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Audio Service',
+      title: 'Simply Music',
       theme: ThemeData.dark(),
-      home: AudioServiceWidget(child: MainScreen()),
+      home: MainScreen(),
     );
   }
 }
